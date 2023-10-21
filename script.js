6cf7a9c219b5900e979318fee74c1570ca95d998
valyusha1999 <valyusha1999@mail.ru>
2023-10-21 10:41:44 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('.input');
+const list = document.querySelector('#items');
+const shopItems = document.createElement ('div');
 
+sendInput.addEventListener('keydown', function(event) {
+    if(event.key == 'Enter') {
+        const text = sendInput.value;
+    }  
+    shopItems.textContent = text;
+    
+    if(text != '') {
+        list.append(shopItems);
+    }
+
+    sendInput.value = '';
+})
+
+
+for (const item of shopItems) {
+    item.addEventListener('click', function () {
+      item.classList.toggle('done');
+    });
+  };
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
